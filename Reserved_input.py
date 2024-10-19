a6a0deb33f29f7193134fcac6b139ac9c5315cf5
Emre ÖZTEMİZ <emreoztemiz.ai.ml@gmail.com>
2024-10-19 13:17:55 +0300

    Add files via upload
    Yazıları tersten yazan ya da tersten yazılanları bu mantık çerçevesinde okuyan bir Python örneği

---
+++ Reserved_input.py
@@ -0,0 +1,10 @@
+def main():
+    print("Hello World")
+    
+    user_input = input("Bir yazı girin: ")
+    reversed_input = user_input[::-1]
+    
+    print(f"Emrex Slogan : {reversed_input}")
+
+if __name__ == "__main__":
+    main()
